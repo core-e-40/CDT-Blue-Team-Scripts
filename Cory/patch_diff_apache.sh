735b2b3da4b0b65b721eef96ce986bc7ee365ce3
core-e-40 <chl2099@g.rit.edu>
2026-02-23 11:13:31 -0500

    cmds to quick fix redis/apache

--- Cory/patch_diff_apache.sh
+++ Cory/patch_diff_apache.sh
@@ -1,1 +1,8 @@
-apt-get -o Dpkg::Options::="--force-confnew" install --reinstall apache2
+# apache
+sudo apt-get -o Dpkg::Options::="--force-confnew" install --reinstall apache2 -y
+sudo systemctl restart apache2
+
+#redis
+sudo apt-get -o Dpkg::Options::="--force-confnew" install --reinstall redis-server -y
+sudo systemctl restart redis-server
+
